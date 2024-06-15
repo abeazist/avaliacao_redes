91fa29b4f3ccac0f17d8cfd2c0612b520ba757fb
beatriz <128046453+abeazist@users.noreply.github.com>
2024-06-15 18:17:11 -0300

    começando js

---
+++ avaliacao_redes/script.js
@@ -0,0 +1,7 @@
+const enderecoIP = document.getElementById('enderecoIP')
+const mascara = document.getElementById('mascara')
+const subrede = document.getElementById('quantSubrede')
+
+function limpar() {
+    
+}
