89dd8b7586cb6f8a3817ab5fc0b2730d1155dfa9
beatriz <128046453+abeazist@users.noreply.github.com>
2024-06-17 08:56:09 -0300

    testando muita coisa

--- avaliacao_redes/script.js
+++ avaliacao_redes/script.js
@@ -3,12 +3,30 @@ const mascara = document.getElementById('mascara');
 const subrede = document.getElementById('quantSubrede');
 const limpar = document.getElementById("limpar")
 
+enderecoIP.addEventListener("input", (evento) => {
+    this.value = removerLetras(this.value)
+    removerLetras();
+    // let letras='abcdefghijklmnopqrstuvwxyz';
+    // let letrasM='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    // if (enderecoIP.value.indexOf(letras) || enderecoIP.value.indexOf(letrasM)) {
+        
+    // }
+    
+    
+    
+    // const ipPattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    // evento.target.value= evento.target.value.replace(/\D\s*/g, "");
+    // if (input.length > 3) input = input.substring(0, 3) + '.' + input.substring(3);
+    // if (input.length > 7) input = input.substring(0, 7) + '.' + input.substring(7);
+    // if (input.length > 11) input = input.substring(0, 11) + '.' + input.substring(11);
 
-enderecoIP.addEventListener("input",(evento)=>{
-    console.log("ola")
-    evento.target.value= evento.target.value.replace(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
-})
+    if (!ipPattern.test(evento.target.value)) {
+        console.log("Endereço IP inválido");
+    } else {
+        console.log("Endereço IP válido");
+    }
+});
 
-limpar.addEventListener("clcik",()=>{
-    console.log('ola')
-})
+function removerLetras(valor) {
+    return valor.replace(/[a-zA-Z]/g, '');
+}
