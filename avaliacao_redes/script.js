bb818fea871bb741973b7e68e662abddf4484cf1
nandadosoob <fer.r.benetti@gmail.com>
2024-06-17 09:23:27 -0300

    funções quase prontas

--- avaliacao_redes/script.js
+++ avaliacao_redes/script.js
@@ -1,5 +1,5 @@
-const enderecoIP = document.getElementById('enderecoIP')
-const mascara = document.getElementById('mascara')
+const enderecoIP = document.getElementById('enderecoIP').value
+const mascara = document.getElementById("mascara")
 const subrede = document.getElementById('quantSubrede')
 
 function limpar() {
@@ -7,7 +7,47 @@ function limpar() {
     //siiim-bea
 }
 
-
 function descobreMascara(){
-    
+    //mascara
+    const mascInicial = (32 - mascara.value);
+    const expoente = Math.pow(2,mascInicial)
+    const qntEndereco = (expoente/subrede.value)
+    const logaritmo = Math.log2(qntEndereco)
+    const mascFinal = (32-logaritmo);
+    console.log(`Macara: ${mascFinal}`)
+
+    //intervalo
+    const intervaloIP = (enderecoIP + qntEndereco)
+    console.log(`Intervalo: ${enderecoIP} -> ${intervaloIP}`)
+
+    //primeiro end
+    const calcPrimeiroEnd = (enderecoIP.value + 1)
+    console.log(`Primeiro end válido: ${calcPrimeiroEnd}`)
+
+    //ultimo end
+    const calcUltimoEnd = (intervaloIP - 1)
+    console.log(`Ultimo end válido: ${calcUltimoEnd}`)
+}
+
+// function enderecoBloco(){
+//     const intervaloIP = (enderecoIP.value + descobreMascara.qntEndereco)
+//     // console.log("Intervalo", enderecoIP "-", intervaloIP)
+//     console.log(`Intervalo: ${enderecoIP} -> ${intervaloIP}`)
+// }
+
+// function primeiroEnd(){
+//     const calcPrimeiroEnd = (enderecoIP.value + 1)
+//     console.log(`Primeiro end válido: ${calcPrimeiroEnd}`)
+// }
+
+// function ultimoEnd(){
+//     const calcUltimoEnd = (intervaloIP - 1)
+//     console.log(`Ultimo end válido: ${calcUltimoEnd}`)
+// }
+
+function calculos(){
+    descobreMascara()
+    // enderecoBloco()
+    // primeiroEnd()
+    // ultimoEnd()
 }
