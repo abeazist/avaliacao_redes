607915c87dc19056b0f0171a13aa126a12821205
beatriz <128046453+abeazist@users.noreply.github.com>
2024-06-21 23:27:40 -0300

    paciencia

--- avaliacao_redes/script.js
+++ avaliacao_redes/script.js
@@ -15,7 +15,7 @@ function descobreMascara(){
     const qntEndereco = (expoente/subrede.value)
     const logaritmo = Math.log2(qntEndereco)
     const mascFinal = (32-logaritmo);
-    console.log(`Macara: ${mascFinal}`)
+    console.log(`Mascara: ${mascFinal}`)
 
     //intervalo
     const intervaloIP = (parseFloat(enderecoIP.value) + parseFloat(qntEndereco))
@@ -53,30 +53,15 @@ function calculos(){
     // ultimoEnd()
 }
 
-// enderecoIP.addEventListener("input", (evento) => {
-//     this.value = removerLetras(this.value)
-//     removerLetras();
-//     // let letras='abcdefghijklmnopqrstuvwxyz';
-//     // let letrasM='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-//     // if (enderecoIP.value.indexOf(letras) || enderecoIP.value.indexOf(letrasM)) {
+enderecoIP.addEventListener("click", () => {
+    this.value = removerLetras(this.value)
+    removerLetras();})
+    // let letras='abcdefghijklmnopqrstuvwxyz';
+    // let letrasM='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    // if (enderecoIP.value.indexOf(letras) || enderecoIP.value.indexOf(letrasM)) {
         
-//     // }
-    
-    
-    
-//     // const ipPattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-//     // evento.target.value= evento.target.value.replace(/\D\s*/g, "");
-//     // if (input.length > 3) input = input.substring(0, 3) + '.' + input.substring(3);
-//     // if (input.length > 7) input = input.substring(0, 7) + '.' + input.substring(7);
-//     // if (input.length > 11) input = input.substring(0, 11) + '.' + input.substring(11);
+    // }
 
-//     if (!ipPattern.test(evento.target.value)) {
-//         console.log("Endereço IP inválido");
-//     } else {
-//         console.log("Endereço IP válido");
-//     }
-// });
-
-// function removerLetras(valor) {
-//     return valor.replace(/[a-zA-Z]/g, '');
-// }
+function removerLetras(valor) {
+    return valor.replace(/[a-zA-Z]/g, '');
+}
