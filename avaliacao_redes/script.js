8aec1d40306e1f6dd3eb2755f52c0717234f960f
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-06-24 09:05:18 -0300

    taindonao

--- avaliacao_redes/script.js
+++ avaliacao_redes/script.js
@@ -1,12 +1,21 @@
-const enderecoIP = document.getElementById('enderecoIP');
 const mascara = document.getElementById('mascara');
 const subrede = document.getElementById('quantSubrede');
+const calcular= document.getElementById('calcular')
+var tabela= document.getElementsByClassName('.tabela')
+var respostas= document.getElementsByClassName('.respostas')
+var voltar= document.getElementById("voltar")
 // const limpar = document.getElementById("limpar")
 
 // function limpar() {
 //     //ta dando pra ver???
 //     //siiim-bea
 // }
+
+calcular.addEventListener('click', calculos(), validarIP(), respostas() )
+voltar.addEventListener('click', function(){
+    tabela.style.display==="flex"
+    respostas.style.display==="none"
+})
 
 function descobreMascara(){
     //mascara
@@ -65,3 +74,66 @@ enderecoIP.addEventListener("click", () => {
 function removerLetras(valor) {
     return valor.replace(/[a-zA-Z]/g, '');
 }
+
+// //Tentativa1 de validar os enderecos
+// function validarIP(ip){
+//     var masc=  /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+//     if (!masc.test(ip)) {
+//         return false
+//     }
+//     return true
+
+// }
+
+// function formatar(ip){
+//     if (!validarIP(ip)){
+//         return  "IP invalido"
+//     }
+
+//     return ip.split(".").map(add => {
+//         return add.replace(/(\d{3})(?=\d)/g, '$1.')
+//     }).join('.')
+// }
+
+
+//Tentativa2 de validar os enderecos 
+function validarIP(ip){
+    var valido= /^\d{1,3}\.\d{1,3}.d{1,3}\.d{1,3}$/
+
+    if(!valido.test(ip)){
+        return false
+    }
+
+    var octeto= ip.split('.')
+
+    for ( lat1=0; i<4; i++){
+        var oct=octeto[i]
+        if (isNaN(oct) || oct<0 || oct >255){
+            return false
+        }
+        //completa com zeros a esquerda se precisar
+        octeto[i]=('000' +oct).slice(-3)
+    }
+    var formato= octeto.join('.')
+
+    return formato
+}
+
+function validoFormato(){
+    const enderecoIP = document.getElementById('enderecoIP').value;
+    var resultado= validarIP(enderecoIP)
+    if (resultado){
+        alert("ip valido")
+    } else {
+        alert("ip invalido")
+    }
+}
+
+function respostas(){
+    if (tabela.style.display==="flex"){
+        tabela.style.display==="none"
+        respostas.style.display==="inline"
+    }
+}
+
+//desisti
